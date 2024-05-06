48fb0313c3e68ea933b3e54dabb555c618ad1d20
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-06 18:34:39 -0400

    test

--- server/src/features/auth/auth.controller.ts
+++ server/src/features/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from '../users/dtos';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,8 +7,8 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register() {
-    return this.authService.register();
+  register(createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
   }
 
   @Post('login')
--- server/src/features/auth/auth.service.ts
+++ server/src/features/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../users/dtos';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  register() {
-    return '';
+  constructor(private usersService: UsersService) {}
+
+  register(createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   login() {
-    return '';
+    return { message: 'login controller' };
   }
 }
