59aa05978cc7a2f0e52620fbe872b1e46ee62477
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-07 12:56:30 -0400

    test

--- server/src/features/users/users.controller.ts
+++ server/src/features/users/users.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../auth/decorators';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Public()
   @Get()
   findAll() {
     return this.usersService.findAll();
