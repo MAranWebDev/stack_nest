b536be0f054502eb9c01acb74dcabe87550d4f7f
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-08 17:11:27 -0400

    test

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,30 +3,32 @@ module.exports = {
   env: { browser: true, node: true, es6: true },
   parser: '@typescript-eslint/parser',
   settings: { 'import/resolver': { typescript: {} } },
-  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts', 'dist', 'node_modules'],
+  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts', 'node_modules', 'dist'],
   plugins: ['react-refresh'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
+    'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
   rules: {
+    /* vite default rules */
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'no-restricted-imports': ['error', { patterns: ['../**', 'src', '@/*/*/*'] }],
-    'import/no-unresolved': 'off',
-    'import/default': 'off',
+
+    /* custom rules */
+    'no-restricted-imports': ['error', { patterns: ['src', '../**', 'features/*/*/*'] }],
+    'import/no-unresolved': ['error', { ignore: ['^@/'] }],
     'import/no-default-export': 'error',
+    'import/default': 'off',
     'import/no-named-as-default-member': 'off',
     'import/order': [
       'error',
       {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
         alphabetize: { order: 'asc', caseInsensitive: true },
         'newlines-between': 'always',
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
       },
     ],
   },
--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,37 +2,35 @@ module.exports = {
   root: true,
   env: { node: true, jest: true },
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  settings: { 'import/resolver': { typescript: {} } },
-  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
+  parserOptions: { project: 'tsconfig.json', tsconfigRootDir: __dirname, sourceType: 'module' },
+  settings: { 'import/resolver': { typescript: true } },
+  ignorePatterns: ['.eslintrc.js', 'node_modules', 'dist'],
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
+    'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
   rules: {
+    /* nestjs default rules */
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-restricted-imports': ['error', { patterns: ['../**', 'src', 'features/*/*/*'] }],
-    'import/no-unresolved': 'off',
-    'import/default': 'off',
+
+    /* custom rules */
+    'no-restricted-imports': ['error', { patterns: ['src', '../**', 'features/*/*/*'] }],
+    'import/no-unresolved': ['error', { ignore: ['^@/'] }],
     'import/no-default-export': 'error',
+    'import/default': 'off',
     'import/no-named-as-default-member': 'off',
     'import/order': [
       'error',
       {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
         alphabetize: { order: 'asc', caseInsensitive: true },
         'newlines-between': 'always',
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
       },
     ],
   },
