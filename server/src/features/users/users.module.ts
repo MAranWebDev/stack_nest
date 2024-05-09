aeaf0f1fd8f44976589d347ba41856c9304af060
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-09 18:09:50 -0400

    test

--- server/src/features/users/users.module.ts
+++ server/src/features/users/users.module.ts
@@ -1,14 +1,19 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { UsersConsumer } from './users.consumer';
 import { UsersController } from './users.controller';
 import { Users, UsersSchema } from './users.schema';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]),
+    BullModule.registerQueue({ name: 'users' }),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersConsumer],
   exports: [UsersService],
 })
 export class UsersModule {}
