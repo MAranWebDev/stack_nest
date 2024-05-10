44d1abb5961c5727807f7a4293529d6264dda32b
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-10 19:13:16 -0400

    test

--- server/src/features/sample/dtos/create-sample.dto.ts
+++ server/src/features/sample/dtos/create-sample.dto.ts
@@ -1,10 +1,11 @@
-import { IsString, Length, Max, Min } from 'class-validator';
+import { IsNumber, IsString, Length, Max, Min } from 'class-validator';
 
 export class CreateSampleDto {
   @IsString()
   @Length(4, 50)
   name: string;
 
+  @IsNumber()
   @Min(0)
   @Max(99)
   quantity: number;
