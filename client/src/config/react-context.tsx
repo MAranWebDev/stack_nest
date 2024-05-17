2ec95830a74ba3d74cacdc12afe56145be1b2b31
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-17 12:50:39 -0400

    test

--- client/src/config/react-context.tsx
+++ client/src/config/react-context.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
-import { AuthProvider } from '@/features/auth';
-import { UsersProvider } from '@/features/users';
+import { AuthProvider } from '@/features/auth/Auth.context';
+import { UsersProvider } from '@/features/users/Users.context';
 
 export const ReactContextProvider = ({ children }: PropsWithChildren) => {
   const providers = [AuthProvider, UsersProvider];
