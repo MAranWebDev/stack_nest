073c0c37d754d9e6906b787da4af49c50cd54b76
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-23 14:38:59 -0400

    test

--- client/src/features/loading/Loading.context.tsx
+++ client/src/features/loading/Loading.context.tsx
@@ -1,18 +1,41 @@
-import { PropsWithChildren, createContext, useState } from 'react';
+import { PropsWithChildren, createContext, useCallback, useState } from 'react';
 
-const contextInitialValues = {
-  isLoading: false,
-  setLoadingState: null as null | React.Dispatch<React.SetStateAction<boolean>>,
+interface StateInitialValuesType {
+  isPending: boolean;
+  isError: boolean;
+  error: Error | null;
+}
+
+interface ContextInitialValuesType extends StateInitialValuesType {
+  changeValues: (props: PropsType) => void;
+}
+
+type PropsType = Partial<StateInitialValuesType>;
+
+const stateInitialValues: StateInitialValuesType = {
+  isPending: false,
+  isError: false,
+  error: null,
+};
+
+const contextInitialValues: ContextInitialValuesType = {
+  ...stateInitialValues,
+  changeValues: () => {},
 };
 
 export const LoadingContext = createContext(contextInitialValues);
 
 export const LoadingProvider = ({ children }: PropsWithChildren) => {
-  const [isLoading, setLoadingState] = useState(false);
+  const [values, setValues] = useState(stateInitialValues);
+
+  const changeValues = useCallback(
+    (props: PropsType) => setValues((prevState) => ({ ...prevState, ...props })),
+    [],
+  );
 
   const providerValues = {
-    isLoading,
-    setLoadingState,
+    ...values,
+    changeValues,
   };
 
   return <LoadingContext.Provider value={providerValues}>{children}</LoadingContext.Provider>;
