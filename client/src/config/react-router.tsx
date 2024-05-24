7df08b5285f960ea8cb1988d0df6ab93b62adb46
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-24 16:53:25 -0400

    test

--- client/src/config/react-router.tsx
+++ client/src/config/react-router.tsx
@@ -1,27 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { PrivateRoute } from '@/features/auth/components';
+import { ROLES } from '@/features/auth/types';
 import { AdminPage } from '@/pages/Admin.page';
 import { ErrorPage } from '@/pages/Error.page';
 import { HomePage } from '@/pages/Home.page';
 import { LoginPage } from '@/pages/Login.page';
 import { RegisterPage } from '@/pages/Register.page';
+import { UnauthorizedPage } from '@/pages/Unauthorized.page';
 
 export const reactRouter = createBrowserRouter([
+  { path: '/', element: <HomePage />, errorElement: <ErrorPage /> },
+  { path: 'register', element: <RegisterPage /> },
+  { path: 'login', element: <LoginPage /> },
+  { path: 'unauthorized', element: <UnauthorizedPage /> },
   {
-    path: '/',
-    element: <HomePage />,
-    errorElement: <ErrorPage />,
+    element: <PrivateRoute role={ROLES.ADMIN} />,
+    children: [{ path: 'admin', element: <AdminPage /> }],
   },
   {
-    path: 'register',
-    element: <RegisterPage />,
+    element: <PrivateRoute role={ROLES.EDITOR} />,
+    children: [],
   },
   {
-    path: 'login',
-    element: <LoginPage />,
-  },
-  {
-    path: 'admin',
-    element: <AdminPage />,
+    element: <PrivateRoute role={ROLES.USER} />,
+    children: [],
   },
 ]);
