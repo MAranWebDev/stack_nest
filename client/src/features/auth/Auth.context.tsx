b01f2b07d65767664ed65d32c34af3cb0ac25da6
unknown <mario.arancibia.jimenez@gmail.com>
2024-05-24 22:26:38 -0400

    test

--- client/src/features/auth/Auth.context.tsx
+++ client/src/features/auth/Auth.context.tsx
@@ -1,17 +1,36 @@
 import { PropsWithChildren, createContext, useState } from 'react';
 
-const contextInitialValues = {
+interface StateInitialValuesType {
+  jwt: string;
+  user: string;
+  userRole: string;
+}
+
+interface ContextInitialValuesType extends StateInitialValuesType {
+  changeValues: (props: PropsType) => void;
+}
+
+type PropsType = Partial<StateInitialValuesType>;
+
+const stateInitialValues: StateInitialValuesType = {
   jwt: '',
   user: '',
   userRole: '',
 };
 
+const contextInitialValues: ContextInitialValuesType = {
+  ...stateInitialValues,
+  changeValues: () => {},
+};
+
 export const AuthContext = createContext(contextInitialValues);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-  const [values] = useState(contextInitialValues);
+  const [values, setValues] = useState(stateInitialValues);
 
-  const providerValues = { ...values };
+  const changeValues = (props: PropsType) => setValues((prevState) => ({ ...prevState, ...props }));
+
+  const providerValues = { ...values, changeValues };
 
   return <AuthContext.Provider value={providerValues}>{children}</AuthContext.Provider>;
 };
