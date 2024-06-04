ae6bdfd987b4ff688ee3681b6449c95eb6e5241b
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-04 16:09:27 -0400

    test

--- client/src/features/nav-bar/navbar.service.ts
+++
@@ -1,9 +0,0 @@
-import { LOCAL_STORAGE } from './utils';
-
-export const navBarService = {
-  persist(isDarkMode: boolean) {
-    isDarkMode
-      ? localStorage.setItem(LOCAL_STORAGE.IS_DARK_MODE, 'true')
-      : localStorage.removeItem(LOCAL_STORAGE.IS_DARK_MODE);
-  },
-};
--- client/src/features/nav-bar/utils/constants/local-storage.ts
+++
@@ -1,3 +0,0 @@
-export const LOCAL_STORAGE = {
-  IS_DARK_MODE: 'is_dark_mode',
-} as const;
