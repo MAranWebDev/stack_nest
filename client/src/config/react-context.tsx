67fbb493ad9749a2d2342a0164ef2c5c6defe02b
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-04 13:06:27 -0400

    test

--- client/src/config/react-context.tsx
+++ client/src/config/react-context.tsx
@@ -2,19 +2,14 @@ import { PropsWithChildren } from 'react';
 
 import { AuthProvider } from '@/features/auth/Auth.context';
 import { LoadingBarProvider } from '@/features/loading-bar/LoadingBar.context';
+import { NavBarProvider } from '@/features/nav-bar/NavBar.context';
 import { UsersProvider } from '@/features/users/Users.context';
 
 export const ReactContextProvider = ({ children }: PropsWithChildren) => {
-  const providers = [AuthProvider, UsersProvider, LoadingBarProvider];
+  const providers = [AuthProvider, UsersProvider, LoadingBarProvider, NavBarProvider];
 
-  return (
-    <>
-      {providers.reduce(
-        (accumulator, Current) => (
-          <Current>{accumulator}</Current>
-        ),
-        children,
-      )}
-    </>
+  return providers.reduce(
+    (accumulator, CurrentProvider) => <CurrentProvider>{accumulator}</CurrentProvider>,
+    children,
   );
 };
