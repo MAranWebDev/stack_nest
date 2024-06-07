e367624c65c542657d628a15f63d01d4f01a22a9
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-07 18:07:02 -0400

    test

--- client/src/features/auth/components/PrivateRoute.tsx
+++ client/src/features/auth/components/PrivateRoute.tsx
@@ -1,12 +1,9 @@
 import { useContext } from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
+import { ROUTES } from '@/constants/routes';
 import { AuthContext } from '@/features/auth/Auth.context';
 import { ROLES } from '@/features/auth/constants';
-
-const ROUTES = {
-  UNAUTHORIZED: '/unauthorized',
-} as const;
 
 interface PropsType {
   roles: ROLES[];
@@ -16,9 +13,10 @@ export const PrivateRoute = ({ roles }: PropsType) => {
   const { userRole } = useContext(AuthContext);
   const currentLocation = useLocation();
 
-  return !roles.includes(userRole as ROLES) ? (
-    <Navigate to={ROUTES.UNAUTHORIZED} state={{ from: currentLocation }} replace />
-  ) : (
-    <Outlet />
-  );
+  if (!userRole) return <Navigate to={ROUTES.LOGIN} state={{ from: currentLocation }} replace />;
+
+  if (!roles.includes(userRole as ROLES))
+    return <Navigate to={ROUTES.UNAUTHORIZED} state={{ from: currentLocation }} replace />;
+
+  return <Outlet />;
 };
