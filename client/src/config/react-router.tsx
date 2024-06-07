051d52479f25304898193825937f4753a462d160
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-07 16:51:38 -0400

    test

--- client/src/config/react-router.tsx
+++ client/src/config/react-router.tsx
@@ -1,33 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { ROUTES } from '@/constants/routes';
 import { PrivateRoute } from '@/features/auth/components';
 import { ROLES } from '@/features/auth/constants';
 import { AdminPage } from '@/pages/Admin.page';
+import { DashboardPage } from '@/pages/Dashboard.page';
 import { ErrorPage } from '@/pages/Error.page';
 import { HomePage } from '@/pages/Home.page';
 import { LoginPage } from '@/pages/Login.page';
 import { RegisterPage } from '@/pages/Register.page';
 import { UnauthorizedPage } from '@/pages/Unauthorized.page';
 
-const fullAuth = [ROLES.ADMIN];
-const writeAuth = [...fullAuth, ROLES.EDITOR];
-const readAuth = [...fullAuth, ...writeAuth, ROLES.USER];
+const adminAuth = [ROLES.ADMIN];
+const defaultAuth = [...adminAuth, ROLES.DEFAULT];
 
 export const reactRouter = createBrowserRouter([
-  { path: '/', element: <HomePage />, errorElement: <ErrorPage /> },
-  { path: 'register', element: <RegisterPage /> },
-  { path: 'login', element: <LoginPage /> },
-  { path: 'unauthorized', element: <UnauthorizedPage /> },
+  { path: ROUTES.HOME, element: <HomePage />, errorElement: <ErrorPage /> },
+  { path: ROUTES.REGISTER, element: <RegisterPage /> },
+  { path: ROUTES.LOGIN, element: <LoginPage /> },
+  { path: ROUTES.UNAUTHORIZED, element: <UnauthorizedPage /> },
   {
-    element: <PrivateRoute roles={fullAuth} />,
-    children: [{ path: 'admin', element: <AdminPage /> }],
+    element: <PrivateRoute roles={adminAuth} />,
+    children: [{ path: ROUTES.ADMIN, element: <AdminPage /> }],
   },
   {
-    element: <PrivateRoute roles={writeAuth} />,
-    children: [],
-  },
-  {
-    element: <PrivateRoute roles={readAuth} />,
-    children: [],
+    element: <PrivateRoute roles={defaultAuth} />,
+    children: [{ path: ROUTES.DASHBOARD, element: <DashboardPage /> }],
   },
 ]);
