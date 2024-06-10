a5239e14bd3939b1e2e82e8f6f4d603f5c8c94d3
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-10 17:52:05 -0400

    test

---
+++ server/src/features/users/services/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { CreateUserDto } from '@/features/users/dtos';
+import { Users } from '@/features/users/schemas';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(Users.name) private usersModel: Model<Users>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.usersModel.create({ ...createUserDto, role: 'DEFAULT' });
+  }
+
+  async findAll() {
+    return this.usersModel.find().select('-password').exec();
+  }
+
+  async findOneByEmail(email: string) {
+    return this.usersModel.findOne({ email }).exec();
+  }
+}
