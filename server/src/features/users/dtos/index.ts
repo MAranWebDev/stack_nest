d32a17ccf22cefd776f085dd06345d7b7642bb59
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-10 18:07:32 -0400

    test

--- server/src/features/users/dtos/index.ts
+++ server/src/features/users/dtos/index.ts
@@ -1,3 +1,5 @@
 export * from './create-user-role.dto';
 export * from './create-user.dto';
 export * from './log-user.dto';
+export * from './update-user-role.dto';
+export * from './update-user.dto';
