159daee5add4d4508d5f86a7965b7fd2925fa15b
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-10 16:36:15 -0400

    test

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,34 @@
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { LogoutButton } from '@/features/auth/components/LogoutButton';
-import { DarkModeSwitch } from '@/features/dark-mode/components/DarkModeSwitch';
+import { ROUTES } from '@/constants/routes';
+import { AuthContext } from '@/features/auth/Auth.context';
+import { DarkModeContext } from '@/features/dark-mode/DarkMode.context';
 
 export const Navbar = () => {
+  const { isDarkMode, handleDarkMode } = useContext(DarkModeContext);
+  const { jwt, handleLogout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <LogoutButton />
-      <DarkModeSwitch />
+      <Switch checked={isDarkMode} onClick={handleDarkMode} />
+      <Button variant="outlined" onClick={() => navigate(ROUTES.REGISTER)}>
+        Register
+      </Button>
+      <Button variant="outlined" onClick={() => navigate(ROUTES.LOGIN)}>
+        Login
+      </Button>
+      <Button
+        sx={{ visibility: jwt ? 'visible' : 'hidden' }}
+        variant="outlined"
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
     </Box>
   );
 };
--- client/src/features/auth/components/index.ts
+++ client/src/features/auth/components/index.ts
@@ -1,6 +1,5 @@
 export * from './FormLayout';
 export * from './LoginForm';
-export * from './LogoutButton';
 export * from './PrivateRoute';
 export * from './PublicRoute';
 export * from './RegisterForm';
