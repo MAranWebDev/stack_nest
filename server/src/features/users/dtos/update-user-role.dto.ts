14087274aed7a3abd7277d13f75a67e607d8ea12
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-14 18:17:07 -0400

    test

--- server/src/features/users/dtos/update-user-role.dto.ts
+++
@@ -1,5 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
-
-import { CreateUserRoleDto } from './create-user-role.dto';
-
-export class UpdateUserRoleDto extends PartialType(OmitType(CreateUserRoleDto, ['_id'] as const)) {}
--- server/src/features/users/services/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './user-roles.service';
-export * from './users.service';
