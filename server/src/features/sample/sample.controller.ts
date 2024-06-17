3090533e99448f890e6b9bc2fd4fa714ae6aa344
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-17 14:19:05 -0400

    test

--- server/src/features/sample/sample.controller.ts
+++ server/src/features/sample/sample.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+import { PERMISSIONS } from '@/features/auth/constants';
+import { RequirePermissions } from '@/features/auth/decorators';
+
 import { CreateSampleDto, UpdateSampleDto } from './dtos';
 import { SampleQueueService, SampleService } from './services';
 
@@ -18,26 +21,31 @@ export class SampleController {
   ) {}
 
   @Post()
+  @RequirePermissions(PERMISSIONS.CREATE_SAMPLE)
   create(@Body() createSampleDto: CreateSampleDto) {
     return this.sampleQueueService.create(createSampleDto);
   }
 
   @Get()
+  @RequirePermissions(PERMISSIONS.READ_SAMPLE)
   findAll() {
     return this.sampleService.findAll();
   }
 
   @Get(':id')
+  @RequirePermissions(PERMISSIONS.READ_SAMPLE)
   findOne(@Param('id') id: string) {
     return this.sampleService.findOne(id);
   }
 
   @Patch(':id')
+  @RequirePermissions(PERMISSIONS.UPDATE_SAMPLE)
   update(@Param('id') id: string, @Body() updateSampleDto: UpdateSampleDto) {
     return this.sampleQueueService.update(id, updateSampleDto);
   }
 
   @Delete(':id')
+  @RequirePermissions(PERMISSIONS.DELETE_SAMPLE)
   remove(@Param('id') id: string) {
     return this.sampleQueueService.remove(id);
   }
