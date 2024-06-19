11738a84d09a4815fdb28646823e23973fb1fec1
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-19 18:11:19 -0400

    test

--- server/src/features/users/controllers/user-permissions.controller.ts
+++ server/src/features/users/controllers/user-permissions.controller.ts
@@ -1,8 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-import { RequirePermissions } from '@/features/auth/decorators';
-import { PERMISSIONS, ROUTES } from '@/features/users/constants';
+import { ROUTES } from '@/features/users/constants';
 import { CreateUserPermissionDto, UpdateUserPermissionDto } from '@/features/users/dtos';
 import { UserPermissionsService } from '@/features/users/services';
 
@@ -13,31 +12,31 @@ export class UserPermissionsController {
   constructor(private readonly userPermissionsService: UserPermissionsService) {}
 
   @Post()
-  @RequirePermissions(PERMISSIONS.CREATE_USER_PERMISSIONS)
+  // @RequirePermissions(PERMISSIONS.CREATE_USER_PERMISSIONS)
   create(@Body() createUserPermissionDto: CreateUserPermissionDto) {
     return this.userPermissionsService.create(createUserPermissionDto);
   }
 
   @Get()
-  @RequirePermissions(PERMISSIONS.READ_USER_PERMISSIONS)
+  // @RequirePermissions(PERMISSIONS.READ_USER_PERMISSIONS)
   findAll() {
     return this.userPermissionsService.findAll();
   }
 
   @Get(':id')
-  @RequirePermissions(PERMISSIONS.READ_USER_PERMISSIONS)
+  // @RequirePermissions(PERMISSIONS.READ_USER_PERMISSIONS)
   findOne(@Param('id') id: string) {
     return this.userPermissionsService.findOne(id);
   }
 
   @Patch(':id')
-  @RequirePermissions(PERMISSIONS.UPDATE_USER_PERMISSIONS)
+  // @RequirePermissions(PERMISSIONS.UPDATE_USER_PERMISSIONS)
   update(@Param('id') id: string, @Body() updateUserPermissionDto: UpdateUserPermissionDto) {
     return this.userPermissionsService.update(id, updateUserPermissionDto);
   }
 
   @Delete(':id')
-  @RequirePermissions(PERMISSIONS.DELETE_USER_PERMISSIONS)
+  // @RequirePermissions(PERMISSIONS.DELETE_USER_PERMISSIONS)
   remove(@Param('id') id: string) {
     return this.userPermissionsService.remove(id);
   }
--- server/src/features/users/controllers/users.controller.ts
+++ server/src/features/users/controllers/users.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-import { RequirePermissions } from '@/features/auth/decorators';
-import { PERMISSIONS, ROUTES } from '@/features/users/constants';
+import { UserGuard } from '@/features/auth/guards';
+import { ROUTES } from '@/features/users/constants';
 import { UpdatePasswordDto, UpdateUserDto } from '@/features/users/dtos';
 import { UsersService } from '@/features/users/services';
 
@@ -13,27 +13,31 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @RequirePermissions(PERMISSIONS.READ_USERS)
+  // @RequirePermissions(PERMISSIONS.READ_USERS)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(UserGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(UserGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Patch(`${ROUTES.PASSWORD}/:id`)
+  @UseGuards(UserGuard)
   updatePassword(@Param('id') id: string, @Body() updatePasswordDto: UpdatePasswordDto) {
     return this.usersService.updatePassword(id, updatePasswordDto);
   }
 
   @Patch(`${ROUTES.STATUS}/:id`)
+  @UseGuards(UserGuard)
   updateStatus(@Param('id') id: string) {
     return this.usersService.updateStatus(id);
   }
