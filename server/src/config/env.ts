6495508382414eb5c041a7dcdcfa24abc7d3b811
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-21 17:54:56 -0400

    test

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,8 +1,8 @@
 export const ENV = () => ({
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   DATABASE_URL: process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
   REDIS_HOST: process.env.REDIS_HOST,
   REDIS_PORT: process.env.REDIS_PORT,
   SERVER_PORT: +process.env.SERVER_PORT,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
 });
