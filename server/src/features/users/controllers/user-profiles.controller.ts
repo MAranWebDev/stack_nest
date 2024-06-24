ba5f5165c214295319c4d45ccc7d429925d92491
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-24 18:07:40 -0400

    test

--- server/src/features/users/controllers/user-profiles.controller.ts
+++ server/src/features/users/controllers/user-profiles.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+import { PERMISSIONS } from '@/features/auth/constants';
+import { RequirePermission } from '@/features/auth/decorators';
 import { ROUTES } from '@/features/users/constants';
 import { CreateUserProfileDto, UpdateUserProfileDto } from '@/features/users/dtos';
 import { UserProfilesService } from '@/features/users/services';
@@ -12,31 +14,31 @@ export class UserProfilesController {
   constructor(private readonly userProfilesService: UserProfilesService) {}
 
   @Post()
-  // @RequirePermissions(PERMISSIONS.CREATE_USER_PROFILES)
+  @RequirePermission(PERMISSIONS.CREATE_USER_PROFILES)
   create(@Body() createUserProfileDto: CreateUserProfileDto) {
     return this.userProfilesService.create(createUserProfileDto);
   }
 
   @Get()
-  // @RequirePermissions(PERMISSIONS.READ_USER_PROFILES)
+  @RequirePermission(PERMISSIONS.READ_USER_PROFILES)
   findAll() {
     return this.userProfilesService.findAll();
   }
 
   @Get(':id')
-  // @RequirePermissions(PERMISSIONS.READ_USER_PROFILES)
+  @RequirePermission(PERMISSIONS.READ_USER_PROFILES)
   findOne(@Param('id') id: string) {
     return this.userProfilesService.findOne(id);
   }
 
   @Patch(':id')
-  // @RequirePermissions(PERMISSIONS.UPDATE_USER_PROFILES)
+  @RequirePermission(PERMISSIONS.UPDATE_USER_PROFILES)
   update(@Param('id') id: string, @Body() updateUserProfileDto: UpdateUserProfileDto) {
     return this.userProfilesService.update(id, updateUserProfileDto);
   }
 
   @Delete(':id')
-  // @RequirePermissions(PERMISSIONS.DELETE_USER_PROFILES)
+  @RequirePermission(PERMISSIONS.DELETE_USER_PROFILES)
   remove(@Param('id') id: string) {
     return this.userProfilesService.remove(id);
   }
