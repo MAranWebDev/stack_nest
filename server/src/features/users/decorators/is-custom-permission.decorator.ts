fbd7f3e09042be332752c9ea939017a08ae54ec4
unknown <mario.arancibia.jimenez@gmail.com>
2024-06-25 16:26:53 -0400

    test

---
+++ server/src/features/users/decorators/is-custom-permission.decorator.ts
@@ -0,0 +1,24 @@
+import {
+  IsArray,
+  IsLowercase,
+  IsString,
+  MaxLength,
+  MinLength,
+  ValidationOptions,
+} from 'class-validator';
+
+interface OptionsType {
+  isArray?: boolean;
+}
+
+export const IsCustomPermission = ({ isArray }: OptionsType = {}) => {
+  return function (object: any, propertyName: string) {
+    const validationOptions: ValidationOptions = { each: isArray };
+
+    isArray && IsArray()(object, propertyName);
+    IsLowercase(validationOptions)(object, propertyName);
+    IsString(validationOptions)(object, propertyName);
+    MinLength(4, validationOptions)(object, propertyName);
+    MaxLength(30, validationOptions)(object, propertyName);
+  };
+};
