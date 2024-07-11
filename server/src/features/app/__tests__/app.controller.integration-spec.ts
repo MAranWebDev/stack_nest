ce2944ec23ae20ef97d674567c9317d198339a7c
unknown <mario.arancibia.jimenez@gmail.com>
2024-07-11 16:19:38 -0400

    test

--- server/src/features/app/__tests__/app.controller.integration-spec.ts
+++ server/src/features/app/__tests__/app.controller.integration-spec.ts
@@ -3,35 +3,38 @@ import { Server } from 'http';
 import { HttpStatus } from '@nestjs/common';
 import * as request from 'supertest';
 
+import { AppController } from '@/features/app/app.controller';
 import { AppService } from '@/features/app/app.service';
 import { destroyTestApp, setupTestApp } from '@/tests/test-app';
 
-const ROUTE = '/';
+describe(`${AppController.name} (integration)`, () => {
+  const ROUTE = '/';
 
-let service: AppService;
-let httpServer: Server;
+  let service: AppService;
+  let httpServer: Server;
 
-beforeAll(async () => {
-  const app = await setupTestApp();
+  beforeAll(async () => {
+    const app = await setupTestApp();
 
-  service = app.get(AppService);
-  httpServer = app.getHttpServer();
-});
+    service = app.get(AppService);
+    httpServer = app.getHttpServer();
+  });
 
-afterAll(async () => {
-  await destroyTestApp();
-});
+  afterAll(async () => {
+    await destroyTestApp();
+  });
 
-beforeEach(() => {
-  jest.restoreAllMocks();
-});
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
 
-it(`GET ${ROUTE} - should call the service, return status 200 and a defined body`, async () => {
-  const spyGetHello = jest.spyOn(service, 'getHello');
+  it(`GET ${ROUTE} - should call the service, return status 200 and a defined body`, async () => {
+    const spyGetHello = jest.spyOn(service, 'getHello');
 
-  const { status, body } = await request(httpServer).get(ROUTE);
+    const { status, body } = await request(httpServer).get(ROUTE);
 
-  expect(spyGetHello).toHaveBeenCalled();
-  expect(status).toBe(HttpStatus.OK);
-  expect(body).toBeDefined();
+    expect(spyGetHello).toHaveBeenCalled();
+    expect(status).toBe(HttpStatus.OK);
+    expect(body).toBeDefined();
+  });
 });
