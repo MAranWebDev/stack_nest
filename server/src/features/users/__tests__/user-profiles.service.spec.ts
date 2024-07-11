da0e03db42e46477cf47fa9d8b3df4f7ec76aaed
unknown <mario.arancibia.jimenez@gmail.com>
2024-07-11 17:03:35 -0400

    test

--- server/src/features/users/__tests__/user-profiles.service.spec.ts
+++ server/src/features/users/__tests__/user-profiles.service.spec.ts
@@ -30,46 +30,42 @@ describe(UserProfilesService.name, () => {
     if (module) await module.close();
   });
 
-  describe('validateAndRemoveDuplicatePermissions', () => {
-    const { READ_USERS } = PERMISSIONS;
+  it.each([
+    {
+      description: 'should return an empty array when given an empty array',
+      input: [],
+      expected: [],
+    },
+    {
+      description: 'should throw BadRequestException when an invalid permission is provided',
+      input: ['invalidPermission'],
+      expected: BadRequestException,
+    },
 
+    {
+      description:
+        'should return an array with unique permissions when given duplicated permissions',
+      input: [PERMISSIONS.READ_USERS, PERMISSIONS.READ_USERS],
+      expected: [PERMISSIONS.READ_USERS],
+    },
+    {
+      description:
+        'should return the same array with unique permissions when all permissions are valid and unique',
+      input: [PERMISSIONS.READ_USERS],
+      expected: [PERMISSIONS.READ_USERS],
+    },
+  ])('validateAndRemoveDuplicatePermissions - $description', ({ input, expected }) => {
     const validatePermissions = (permissions: string[]) =>
       service['validateAndRemoveDuplicatePermissions'](permissions);
 
-    it.each([
-      {
-        description: 'should return an empty array when given an empty array',
-        input: [],
-        expected: [],
-      },
-      {
-        description: 'should throw BadRequestException when an invalid permission is provided',
-        input: ['invalidPermission'],
-        expected: BadRequestException,
-      },
+    if (expected === BadRequestException) {
+      const testFunction = () => validatePermissions(input);
 
-      {
-        description:
-          'should return an array with unique permissions when given duplicated permissions',
-        input: [READ_USERS, READ_USERS],
-        expected: [READ_USERS],
-      },
-      {
-        description:
-          'should return the same array with unique permissions when all permissions are valid and unique',
-        input: [READ_USERS],
-        expected: [READ_USERS],
-      },
-    ])('$description', ({ input, expected }) => {
-      if (expected === BadRequestException) {
-        const testFunction = () => validatePermissions(input);
+      expect(testFunction).toThrow(expected);
+    } else {
+      const result = validatePermissions(input);
 
-        expect(testFunction).toThrow(expected);
-      } else {
-        const result = validatePermissions(input);
-
-        expect(result).toEqual(expected);
-      }
-    });
+      expect(result).toEqual(expected);
+    }
   });
 });
